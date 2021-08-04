065c076d2b95973375b8f5243e72e5a6ee2080ed
Dzakhveisha <Dzakhveisha@gmail.com>
2021-08-04 18:16:13 +0300

    Language Filter

--- src/main/java/com/epam/jwd/web/filter/LanguageFilter.java
+++ src/main/java/com/epam/jwd/web/filter/LanguageFilter.java
@@ -1,17 +1,42 @@
 package com.epam.jwd.web.filter;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Map;
 
 public class LanguageFilter implements Filter {
-    public void init(FilterConfig config) throws ServletException {
-    }
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
 
-    public void destroy() {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        String lang = request.getParameter("lang");
+        if (lang != null) {
+            Map<String, String[]> parameterMap = request.getParameterMap();
+            StringBuilder requestString = new StringBuilder(request.getContextPath() + "/controller?");
+            for (Map.Entry<String, String[]> param : parameterMap.entrySet()) {
+                String key = param.getKey();
+                String[] value = param.getValue();
+                if (!key.equals("lang")) {
+                    requestString.append(key).append("=").append(value[0]).append("&");
+                }
+            }
+            requestString.deleteCharAt(requestString.length() - 1);
+            HttpSession session = request.getSession();
+            session.setAttribute("language", lang);
+            response.sendRedirect(requestString.toString());
+            return;
+        }
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
-        chain.doFilter(request, response);
+    public void destroy() {
+
     }
 }
--- src/main/webapp/WEB-INF/jsp/main.jsp
+++ src/main/webapp/WEB-INF/jsp/main.jsp
@@ -1,6 +1,7 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page language="java" contentType="text/html; charset=utf-8" %>
 <%@ page pageEncoding="utf-8" %>
+
 <html>
 <head>
     <title>Main</title>
